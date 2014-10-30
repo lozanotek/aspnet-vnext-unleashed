38ae10712e74df614593451040854f2a75a8db94
jglozano <javier@lozanotek.com>
2014-10-29 22:47:15 -0500

    Added samples into the repository

---
+++ src/BugTracker/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Routing;
+
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        /* Error page middleware displays a nice formatted HTML page for any unhandled exceptions in the request pipeline.
+        * Note: ErrorPageOptions.ShowAll to be used only at development time. Not recommended for production.
+        */
+        app.UseErrorPage(ErrorPageOptions.ShowAll);
+
+        app.UseServices(services =>
+        {
+            //Add all MVC related services to IoC.
+            services.AddMvc();
+
+            //Add all SignalR related services to IoC.
+            services.AddSignalR();
+        });
+
+        //Configure SignalR
+        app.UseSignalR();
+
+        //Serves static files in the application.
+        app.UseFileServer();
+
+        //Configure WebFx
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                null,
+                "{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
+            routes.MapRoute(
+                null,
+                "api/{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
+            routes.MapRoute(
+                null,
+                "api/{controller}",
+                new { controller = "Home" });
+        });
+    }
+}
